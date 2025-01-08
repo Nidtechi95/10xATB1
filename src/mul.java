423abfc0c6054bcc9ae4dbdb1da17af1960d71f8
Nidtechi95 <nidhigurjarmca@gmail.com>
2025-01-08 17:34:24 +0530

    finalpush8th jan

---
+++ src/mul.java
@@ -0,0 +1,12 @@
+public class mul
+{
+    public static void main(String[] args) {
+        int i,j,mul;
+        i= 20;
+        j=30;
+        mul=i*j;
+        System.out.println(mul);
+
+
+    }
+}
---
+++ src/plus.java
@@ -0,0 +1,11 @@
+public class plus
+{
+    public static void main(String[] args) {
+        int i,j,sum;
+        i=10;
+        j=20;
+        sum = i+j;
+        System.out.println(sum);
+
+    }
+}
